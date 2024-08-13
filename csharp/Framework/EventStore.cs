9e06a6e4877f144bb3c5488a53f0b07fcfaabf0a
Ruben Mertens <ruben@cerm.be>
2024-08-13 10:34:20 +0200

    wip append event func

--- csharp/Framework/EventStore.cs
+++ csharp/Framework/EventStore.cs
@@ -3,16 +3,36 @@ using Npgsql;
 
 namespace Framework;
 
-
-
 public class EventStore(NpgsqlConnection connection)
 {
-    public async Task AppendEvent(object @event, Guid streamId, long? expectedVersion)
+    public async Task AppendEvent(object @event, string eventType, Guid streamId, long? expectedVersion)
     {
-        
+        await CheckVersion(streamId, expectedVersion);
+        var appendEventSql = """
+                             DO $$ 
+                             BEGIN
+                                DECLARE stream_version int;
+                                SELECT position INTO stream_version;
+                                    FROM event_store
+                                    WHERE stream_id = :streamId
+                                    ORDER BY position DESC
+                                    LIMIT 1;
+                                    
+                                IF :expected_version IS NOT NULL AND stream_version != :expected_version THEN
+                                    RAISE EXCEPTION 'WrongExpectedVersion';
+                                    RETURN;
+                                END IF;
+                                
+                                stream_version := stream_version + 1;
+                                
+                                INSERT INTO event_store 
+                                    (stream_id, position, event_type, event_data, metadata)
+                             END $$;
+                             """;
     }
-    
-    private async Task GetLastPositionOf(Guid streamId)
+
+
+    private async Task<long?> GetLastPositionOf(Guid streamId)
     {
         await using var cmd = new NpgsqlCommand(@"
             SELECT position FROM event_store
@@ -21,13 +41,14 @@ public class EventStore(NpgsqlConnection connection)
             LIMIT 1
         ", connection);
         cmd.Parameters.AddWithValue("streamId", streamId);
-        return await cmd.ExecuteReaderAsync();
+        var position = await cmd.ExecuteScalarAsync();
+        return (long?)position;
     }
-    
-    private async Task CheckVersion( Guid streamId, long? expectedVersion)
+
+    private async Task CheckVersion(Guid streamId, long? expectedVersion)
     {
-        var lastEvent = await GetLastPositionOf(streamId);
-        if (lastEvent == null)
+        var lastPositionInStream = await GetLastPositionOf(streamId);
+        if (lastPositionInStream == null)
         {
             if (expectedVersion != null && expectedVersion != 0)
             {
@@ -36,13 +57,14 @@ public class EventStore(NpgsqlConnection connection)
         }
         else
         {
-            if (expectedVersion != null && lastEvent.Position != expectedVersion)
+            if (expectedVersion != null && lastPositionInStream != expectedVersion)
             {
-                throw new Exception("Version conflict");
+                throw new Exception(
+                    $"Version conflict, expected version {expectedVersion} but found {lastPositionInStream}");
             }
         }
     }
-    
+
     private async Task CreateEventStoreTable()
     {
         await using var cmd = new NpgsqlCommand(@"
