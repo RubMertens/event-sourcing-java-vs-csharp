4a42ac55ce53eb3401939cf452fc82fc2248e750
toonvermeulen <Toon.vermeulen@remmicom.be>
2024-08-13 09:21:35 +0200

    Init spring java

---
+++ java/paintagrid/src/main/java/be/infosupport/eventsourcingtalk/paintagrid/PaintagridApplication.java
@@ -0,0 +1,13 @@
+package be.infosupport.eventsourcingtalk.paintagrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintagridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintagridApplication.class, args);
+	}
+
+}
